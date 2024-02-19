52189282e8404a801a049594e87390d32cdb4af9
Johannes Schlatow <johannes.schlatow@genode-labs.com>
2024-02-19 16:25:08 +0100

    make latency plot optional

--- schedule.py
+++ schedule.py
@@ -7,8 +7,9 @@ from sched import scheduler
 from plot  import plot
 
 parser = argparse.ArgumentParser(description="Simulate CPU scheduler")
-parser.add_argument("filename",    type=str)
+parser.add_argument("filename", type=str)
 parser.add_argument("--policy", choices=["rr", "stride"], default="rr")
+parser.add_argument("--plot_latencies", action="store_true")
 
 args = parser.parse_args()
 
@@ -21,8 +22,9 @@ elif args.policy == "stride":
 
 s.execute(trace)
 
-# plot response time distribution
-plot.LatencyPlot(s.response_times).show()
+if args.plot_latencies:
+    # plot response time distribution
+    plot.LatencyPlot(s.response_times).show()
 
 # plot virtual time and fairness
 plot.FairnessPlot(s.trace).show()
